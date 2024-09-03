080c911b8f1617a9b8c716e9a7aadba186a27c1f
Gerti23 <gertibajo17@gmail.com>
2024-09-03 14:55:23 +0000

    update

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-text = "object-oriented programming with Python"
-print(text)
+print("object-oriented programming with Python")
