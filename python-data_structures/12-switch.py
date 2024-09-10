e1d4242bbd1df7fedba955a398986b62d128baec
Gerti23 <gertibajo17@gmail.com>
2024-09-10 11:12:11 +0000

    update

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-"""
-This script demonstrates swapping the values of two variables and printing the result.
-
-The script initializes two variables, `a` and `b`, with the values 89 and 10, respectively.
-It t "a=<value> - b=<value>".
-
-Example:
-    $ ./swap_variables.py
-    a=10 - b=89
-"""
-
 a = 89
 b = 10
 a, b = b, a
