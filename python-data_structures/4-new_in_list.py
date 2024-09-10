d8b405efff0e1681a5adc0aecccd2614ca81c9ab
Gerti23 <gertibajo17@gmail.com>
2024-09-10 10:48:16 +0000

    update

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+def new_in_list(my_list, idx, element):
+    # Create a copy of the original list
+    new_list = my_list[:]
+    # Check if the index is within the valid range
+    if 0 <= idx < len(my_list):
+        new_list[idx] = element
+    # Return the new list
+    return new_list
+
+# Example usage
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = new_in_list(my_list, idx, new_element)
+
+    print(new_list)
+    print(my_list)
