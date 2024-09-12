999522f832ebca0451bd3829f57cfc83e7fea0a5
Gerti23 <gertibajo17@gmail.com>
2024-09-12 14:00:01 +0000

    push

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     unique_numbers = set(my_list)  # Using a set to store unique numbers
     return sum(unique_numbers)  # Summing up the unique numbers
 
-# Example usage
+
 if __name__ == "__main__":
     my_list = [1, 2, 3, 1, 4, 2, 5]
     result = uniq_add(my_list)
