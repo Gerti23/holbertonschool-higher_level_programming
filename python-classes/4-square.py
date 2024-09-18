f0641b97be54de50d83a4fe557811afc1bfe0e55
Gerti23 <gertibajo17@gmail.com>
2024-09-18 13:54:13 +0000

    update

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    "Represent a square."""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        """Initializes the data."""
+        if type(value) is not int:
+            print("size must be an integer", end="")
+            raise TypeError
+        if value < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
