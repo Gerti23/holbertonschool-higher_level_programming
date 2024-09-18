6bda3e676278dc0d6bb9cbff5919a36a3475decc
Gerti23 <gertibajo17@gmail.com>
2024-09-18 13:39:49 +0000

    update

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A square class that represents an actual square"""
+
+class Square():
+    """ An empty square class
+
+        Args:
+            size (int): The size of the square
+
+    """
+
+    def __init__(self, size):
+        self.size = size
