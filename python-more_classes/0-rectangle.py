84c4b11f9b2dea71278080f14a9a8dc1eb2e113e
Gerti23 <gertibajo17@gmail.com>
2024-09-19 10:22:09 +0000

    update

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+
+Classes:
+    Rectangle: An empty class that defines a square.
+"""
+class Rectangle(object):
+ """
+    A class used to represent a Rectangle.
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    None
+    """
+    pass
