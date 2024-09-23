25afec4aa96a3bfdb4eab963d09173f0902d5fb8
Gerti23 <gertibajo17@gmail.com>
2024-09-23 10:34:48 +0000

    push

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 def lookup(obj):
-        return obj.__dict__
+    """
+    Returns the dictionary representation of an object's attributes.
+
+    Args:
+        obj: The object whose attributes are to be returned.
+
+    Returns:
+        dict: A dictionary containing the object's attributes and their values.
+    """
+    return obj.__dict__
