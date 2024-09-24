967bab92e94b730b6e6b20be48ef36ef2caecb9c
Gerti23 <gertibajo17@gmail.com>
2024-09-24 09:52:30 +0000

    update

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """BaseGeometry class"""
+"""Defines a class BaseGeometry."""
 
+
+class BaseGeometry:
+    """Represents a base geometry object."""
     def area(self):
-        """Raises an exception with a message"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is a positive integer"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
