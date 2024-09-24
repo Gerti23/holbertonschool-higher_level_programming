5db43cb4b235e47d79f2e5cca31e73bba50ac3e9
Gerti23 <gertibajo17@gmail.com>
2024-09-24 09:12:14 +0000

    update

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class:
+
+    A simple base class for geometric operations.
+    Currently, it does not implement any methods or attributes.
+    """
+    pass
