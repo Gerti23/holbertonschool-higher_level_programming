569a3114280b1ae560ccd1164255517cf550b4b1
Gerti23 <gertibajo17@gmail.com>
2024-09-24 09:09:58 +0000

    update

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Check if an object is an instance of a subclass of a specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Determine if obj is an instance of a subclass of a_class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to match against.
+
+    Returns:
+        True if obj is an instance of a subclass of a_class, else False.
+    """
+    if isinstance(obj, a_class) and not type(obj) is a_class:
+        return True
+    return False
