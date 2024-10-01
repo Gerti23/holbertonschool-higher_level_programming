69d7efac29be939dcdc4960cfa2123d6027acbf4
Gerti23 <gertibajo17@gmail.com>
2024-10-01 10:46:55 +0000

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that contains the function read_file."""
+
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
