3ad3daa9fcde4eeca7a5698965843c04f77d094d
Gerti23 <gertibajo17@gmail.com>
2024-10-22 11:57:24 +0000

    update

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,6 @@
-SHOW DATABASES;
+-- This script lists all databases in the MySQL server
+-- The output will be sorted alphabetically
+
+SELECT SCHEMA_NAME AS 'Database'
+FROM information_schema.SCHEMATA
+ORDER BY SCHEMA_NAME;
