7372e5d3c2280bcc25606b7f66b62b2b3d45f62d
Gerti23 <gertibajo17@gmail.com>
2024-10-28 15:29:52 +0000

    update

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
 
-CREATE USER IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Write a script that creates the MySQL server user user_0d_2
 
