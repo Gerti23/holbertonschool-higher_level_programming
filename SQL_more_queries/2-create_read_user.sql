3fb65dd6121b0346832bb8d6a88faf8f1dccd659
Gerti23 <gertibajo17@gmail.com>
2024-10-28 15:28:31 +0000

    update

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,11 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE USER IF NOT EXISTS hbtn_0d_2;
 
--- List privileges for user_0d_2
+-- Write a script that creates the MySQL server user user_0d_2
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Write a script that gives the user user_0d_2 read privilege
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
