7cae83757b799b67ce7e2886b8474d30442dfb14
Gerti23 <gertibajo17@gmail.com>
2024-10-28 15:17:42 +0000

    update

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,11 @@
--- Script to create user 'user_0d_1' with all privileges
+-- Attempt to create user 'user_0d_1' with the specified password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to user_0d_1
+-- If the user already exists, update the password to 'user_0d_1_pwd'
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on the server to 'user_0d_1'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Flush privileges to apply changes
+-- Apply privilege changes
 FLUSH PRIVILEGES;
